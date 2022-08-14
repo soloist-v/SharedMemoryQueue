4cc0ac686f36c32071f16e2abafe1fc96f52842a
soloist <w1053904672@163.com>
2022-08-14 13:50:48 +0800

    update

--- SharedArray.py
+++ SharedArray.py
@@ -3,35 +3,9 @@ import numpy as np
 from ctypes import c_uint8, sizeof
 from typing import Union
 import ctypes as ct
-import os
-import secrets
 from numpy import ndarray, prod
 from shared import SharedMemory, read_data, write_data
 from shared_memory_record import SharedMemoryRecorder
-
-base_dir = os.path.abspath(os.path.dirname(__file__))
-_SHM_SAFE_NAME_LENGTH = 14
-_SHM_NAME_PREFIX = 'wnsm_'
-
-
-def make_filename():
-    "Create a random filename for the shared memory object."
-    # number of random bytes to use for name
-    nbytes = (_SHM_SAFE_NAME_LENGTH - len(_SHM_NAME_PREFIX)) // 2
-    assert nbytes >= 2, '_SHM_NAME_PREFIX too long'
-    name = _SHM_NAME_PREFIX + secrets.token_hex(nbytes)
-    assert len(name) <= _SHM_SAFE_NAME_LENGTH
-    return name
-
-
-def _make_filename():
-    "Create a random filename for the shared memory object."
-    # number of random bytes to use for name
-    nbytes = (_SHM_SAFE_NAME_LENGTH - len(_SHM_NAME_PREFIX)) // 2
-    assert nbytes >= 2, '_SHM_NAME_PREFIX too long'
-    name = _SHM_NAME_PREFIX + secrets.token_hex(nbytes)
-    assert len(name) <= _SHM_SAFE_NAME_LENGTH
-    return name
 
 
 class _Nop:
@@ -97,9 +71,6 @@ class Array(NDArray, SharedMemoryRecorder):
     def copy(self, order='C'):
         # return np.ndarray(self.shape, self.dtype, self.data).copy(order)
         return np.copy(self.data, order)
-
-    # def __del__(self):
-    #     self.close()
 
 
 class Value:
